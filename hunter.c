c2d6a544ef48df956cb579e8c7195ad1984cbdfc
f-atweri <151798533+f-atweri@users.noreply.github.com>
2023-11-23 15:11:08 -0500

    update hunter.c
    added a initHunter function

--- hunter.c
+++ hunter.c
@@ -1,3 +1,21 @@
 #include "defs.h"
 
 // function to initialize hunter
+Hunter* initializeHunter(char* name, EvidenceType evidenceType, Room* startingRoom, EvidenceNode* sharedEvidenceList) {
+    Hunter* newHunter = (Hunter*)malloc(sizeof(Hunter)); // allocate memory for the new hunter
+
+    // maybe here we can have some if statements to take care of cases when some parameters might be NULL but we can save that for the end when we have the basic stuff done
+
+    // initial values for the hunter
+    strncpy(newHunter->name, name, MAX_STR - 1);  // copy the name??
+    newHunter->evidence_type = evidenceType;
+    newHunter->curr_room = startingRoom; // placeholder because the startingRoom for a hunter should be the van
+    newHunter->shared_evidence_list = sharedEvidenceList;
+    newHunter->fear = 0; // fear when they start hunting is initially 0
+    newHunter->boredom = 0; // boredom when they start hunting is initially 0
+
+    // log the initialization
+    l_hunterInit(newHunter->name, newHunter->evidence_type);
+
+    return newHunter;
+}
