f7431f434d0dc6cf1f87e42cc86cdb8edee2e503
f-atweri <151798533+f-atweri@users.noreply.github.com>
2023-12-02 17:02:59 -0500

    moving helper function

--- house.c
+++ house.c
@@ -28,15 +28,6 @@ void populateRooms(HouseType* house, const char* filename) {
     fclose(file);
 }
 
-// helper function to replace underscores with spaces in a string
-void replaceUnderscoreWithSpace(char* str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == '_') {
-            str[i] = ' ';
-        }
-    }
-}
-
 void cleanupHouse(HouseType* house) {
     cleanupRoomList(&house->rooms);
     cleanupEvidenceList(&house->evidence_list);
