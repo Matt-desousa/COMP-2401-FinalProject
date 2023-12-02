2d16d7267dd4849ab618a3a8c10dc4fa010e9969
f-atweri <151798533+f-atweri@users.noreply.github.com>
2023-12-02 16:45:51 -0500

    writing/appending to text file stuff

--- logger.c
+++ logger.c
@@ -6,10 +6,15 @@
     in: equipment - the hunter's equipment
 */
 void l_hunterInit(char* hunter, enum EvidenceType equipment, char* color) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     char ev_str[MAX_STR];
     evidenceToString(equipment, ev_str);
-    printf("%s[HUNTER INIT] [%s] is a [%s] hunter\n", color, hunter, ev_str);    
+    printf("%s[HUNTER INIT] [%s] is a [%s] hunter\n", color, hunter, ev_str); 
+
+    fprintf(log_file, "[HUNTER INIT] [%s] is a [%s] hunter\n", hunter, ev_str); // append to game_log.txt
+    fclose(log_file);  // close file 
 }
 
 /*
@@ -18,8 +23,13 @@ void l_hunterInit(char* hunter, enum EvidenceType equipment, char* color) {
     in: room - the room name to log
 */
 void l_hunterMove(char* hunter, char* room, char* color) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     printf("%s[HUNTER MOVE] [%s] has moved into [%s]\n", color, hunter, room);
+
+    fprintf(log_file, "[HUNTER MOVE] [%s] has moved into [%s]\n", hunter, room); // append to game_log.txt
+    fclose(log_file); // close file 
 }
 
 /*
@@ -28,21 +38,30 @@ void l_hunterMove(char* hunter, char* room, char* color) {
     in: reason - the reason for exiting, either LOG_FEAR, LOG_BORED, or LOG_EVIDENCE
 */
 void l_hunterExit(char* hunter, enum LoggerDetails reason, char* color) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     printf("%s[HUNTER EXIT] [%s] exited because ", color, hunter);
+    fprintf(log_file, "[HUNTER EXIT] [%s] exited because ", hunter); // append to game_log.txt
     switch (reason) {
         case LOG_FEAR:
             printf("%s[FEAR]\n", color);
+            fprintf(log_file, "[FEAR]\n"); // append to game_log.txt
             break;
         case LOG_BORED:
             printf("%s[BORED]\n", color);
+            fprintf(log_file, "[BORED]\n"); // append to game_log.txt
             break;
         case LOG_EVIDENCE:
             printf("%s[EVIDENCE]\n", color);
+            fprintf(log_file, "[EVIDENCE]\n"); // append to game_log.txt
             break;
         default:
             printf("%s[UNKNOWN]\n", color);
+            fprintf(log_file, "[UKNOWN]\n"); // append to game_log.txt
     }
+
+    fclose(log_file); // close file 
 }
 
 /*
@@ -51,18 +70,26 @@ void l_hunterExit(char* hunter, enum LoggerDetails reason, char* color) {
     in: result - the result of the review, either LOG_SUFFICIENT or LOG_INSUFFICIENT
 */
 void l_hunterReview(char* hunter, enum LoggerDetails result, char* color) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     printf("%s[HUNTER REVIEW] [%s] reviewed evidence and found ", color, hunter);
+    fprintf(log_file, "[HUNTER REVIEW] [%s] reviewed evidence and found ", hunter); // append to game_log.txt
     switch (result) {
         case LOG_SUFFICIENT:
             printf("%s[SUFFICIENT]\n", color);
+            fprintf(log_file, "[SUFFICIENT]\n"); // append to game_log.txt
             break;
         case LOG_INSUFFICIENT:
             printf("%s[INSUFFICIENT]\n", color);
+            fprintf(log_file, "[INSUFFICIENT]\n"); // append to game_log.txt
             break;
         default:
             printf("%s[UNKNOWN]\n", color);
+            fprintf(log_file, "[UKNOWN]\n"); // append to game_log.txt
     }
+
+    fclose(log_file); // close file 
 }
 
 /*
@@ -72,21 +99,15 @@ void l_hunterReview(char* hunter, enum LoggerDetails result, char* color) {
     in: room - the room name to log
 */
 void l_hunterCollect(char* hunter, enum EvidenceType evidence, char* room, char* color) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     char ev_str[MAX_STR];
     evidenceToString(evidence, ev_str);
     printf("%s[HUNTER EVIDENCE] [%s] found [%s] in [%s] and [COLLECTED]\n", color, hunter, ev_str, room);
-}
 
-/*
-    Logs the hunter NOT finding/collecting evidence.
-    in: hunter - the hunter name to log
-    in: evidence - the evidence type to log
-    in: room - the room name to log
-*/
-void l_hunterCollectNone(char* hunter, char* room, char* color) {
-    if (!LOGGING) return;
-    printf("%s[HUNTER EVIDENCE] [%s] found [NONE] in [%s]\n", color, hunter, room);
+    fprintf(log_file, "[HUNTER EVIDENCE] [%s] found [%s] in [%s] and [COLLECTED]\n", hunter, ev_str, room); // append to game_log.txt
+    fclose(log_file); // close file 
 }
 
 /*
@@ -94,14 +115,19 @@ void l_hunterCollectNone(char* hunter, char* room, char* color) {
     in: room - the room name to log
 */
 void l_ghostMove(char* room, int success) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     if (success){
         printf("\033[0m[GHOST MOVE] Ghost has moved into [%s]\n", room);
+        fprintf(log_file, "[GHOST MOVE] Ghost has moved into [%s]\n", room); // append to game_log.txt
     }
     else{
         printf("\033[0m[GHOST MOVE] Ghost tried to move into [%s] but was blocked\n", room);
+        fprintf(log_file, "[GHOST MOVE] Ghost tried to move into [%s] but was blocked\n", room); // append to game_log.txt
     }
     
+    fclose(log_file); // close file 
 }
 
 /*
@@ -109,21 +135,30 @@ void l_ghostMove(char* room, int success) {
     in: reason - the reason for exiting, either LOG_FEAR, LOG_BORED, or LOG_EVIDENCE
 */
 void l_ghostExit(enum LoggerDetails reason) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     printf("\033[0m[GHOST EXIT] Exited because ");
+    fprintf(log_file, "[GHOST EXIT] Exited because "); // append to game_log.txt
     switch (reason) {
         case LOG_FEAR:
             printf("\033[0m[FEAR]\n");
+            fprintf(log_file, "[FEAR]\n"); // append to game_log.txt
             break;
         case LOG_BORED:
             printf("\033[0m[BORED]\n");
+            fprintf(log_file, "[BORED]\n"); // append to game_log.txt
             break;
         case LOG_EVIDENCE:
             printf("\033[0m[EVIDENCE]\n");
+            fprintf(log_file, "[EVIDENCE]\n"); // append to game_log.txt
             break;
         default:
             printf("\033[0m[UNKNOWN]\n");
+            fprintf(log_file, "[UNKNOWN]\n"); // append to game_log.txt
     }
+
+    fclose(log_file); // close file 
 }
 
 /*
@@ -132,10 +167,15 @@ void l_ghostExit(enum LoggerDetails reason) {
     in: room - the room name to log
 */
 void l_ghostEvidence(enum EvidenceType evidence, char* room) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     char ev_str[MAX_STR];
     evidenceToString(evidence, ev_str);
     printf("\033[0m[GHOST EVIDENCE] Ghost left [%s] in [%s]\n", ev_str, room);
+
+    fprintf(log_file, "[GHOST EVIDENCE] Ghost left [%s] in [%s]\n", ev_str, room); // append to game_log.txt
+    fclose(log_file); // close file 
 }
 
 /*
@@ -144,8 +184,13 @@ void l_ghostEvidence(enum EvidenceType evidence, char* room) {
     in: room - the room name that the ghost is starting in
 */
 void l_ghostInit(enum GhostClass ghost, char* room) {
+    FILE* log_file = fopen(LOG_FILE, "a"); // open game_log.txt
+
     if (!LOGGING) return;
     char ghost_str[MAX_STR];
     ghostToString(ghost, ghost_str);
     printf("\033[0m[GHOST INIT] Ghost is a [%s] in room [%s]\n", ghost_str, room);
+
+    fprintf(log_file, "[GHOST INIT] Ghost is a [%s] in room [%s]\n", ghost_str, room); // append to game_log.txt
+    fclose(log_file); // close file 
 }
